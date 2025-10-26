329d23706902d4e9ac4a70d8fea5b4c6018e557e
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-10-26 13:31:15 -0700

    Optimize loading animations using videos

--- src/lib/components/LoadingGif.svelte
+++ src/lib/components/LoadingGif.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
-  import snifferWalk from "$lib/assets/Sniffer_walk_pixel_art.gif";
-  import sniffletPlop from "$lib/assets/Snifflet_plop_pixel_art.gif";
-  import sniffletSniff from "$lib/assets/Snifflet_sniff_pixel_art.gif";
+  import snifferWalkGif from "$lib/assets/Sniffer_walk_pixel_art.gif";
+  import sniffletPlopGif from "$lib/assets/Snifflet_plop_pixel_art.gif";
+  import sniffletSniffGif from "$lib/assets/Snifflet_sniff_pixel_art.gif";
+  import snifferWalkWebm from "$lib/assets/Sniffer_walk_pixel_art.webm";
+  import sniffletPlopWebm from "$lib/assets/Snifflet_plop_pixel_art.webm";
+  import sniffletSniffWebm from "$lib/assets/Snifflet_sniff_pixel_art.webm";
+  import snifferWalkMp4 from "$lib/assets/Sniffer_walk_pixel_art.mp4";
+  import sniffletPlopMp4 from "$lib/assets/Snifflet_plop_pixel_art.mp4";
+  import sniffletSniffMp4 from "$lib/assets/Snifflet_sniff_pixel_art.mp4";
 
   interface Props {
     text?: string | undefined;
@@ -9,24 +15,36 @@
 
   let { text }: Props = $props();
 
-  const gifs = [snifferWalk, sniffletPlop, sniffletSniff];
+  const gifs = [
+    {
+      gif: snifferWalkGif,
+      webm: snifferWalkWebm,
+      mp4: snifferWalkMp4,
+    },
+    {
+      gif: sniffletPlopGif,
+      webm: sniffletPlopWebm,
+      mp4: sniffletPlopMp4,
+    },
+    {
+      gif: sniffletSniffGif,
+      webm: sniffletSniffWebm,
+      mp4: sniffletSniffMp4,
+    },
+  ];
 
-  export function getRandomLoadingGif(): string {
+  export function getRandomLoadingGif(): { gif: string; webm: string; mp4: string } {
     const randomIndex = Math.floor(Math.random() * gifs.length);
     return gifs[randomIndex];
   }
 </script>
 
 <div>
-  <img src={getRandomLoadingGif()} alt="Loading..." class="pixel-art h-24 w-24" />
+  <video autoplay loop muted playsinline class="h-24 w-24">
+    <source src={getRandomLoadingGif().webm} type="video/webm" />
+    <source src={getRandomLoadingGif().mp4} type="video/mp4" />
+  </video>
   {#if text}
     <div class="animate-pulse text-sm text-neutral-500">{text}</div>
   {/if}
 </div>
-
-<style>
-  .pixel-art {
-    image-rendering: pixelated;
-    object-fit: contain;
-  }
-</style>
